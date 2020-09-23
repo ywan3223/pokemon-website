e45ed67f5002a9a0e7df97d43a3c57cafac2cbdc
ywan3223 <wwwinniewang@gmail.com>
2020-09-23 02:47:59 -0400

    add basic js code for search box

--- searchBox.js
+++ searchBox.js
@@ -1,25 +1,43 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//make a list of pokemon so my js function can search through by number or name       
+var pokemonNameList = [
+    " #001 Bulbasaur",
+    " #002 Ivysaur",
+    " #003 Venusaur",
+    " #004 Charmander",
+    " #005 Charmeleon",
+    " #006 Charizard",
+    " #007 Squirtle",
+    " #008 Wartortle",
+    " #009 Blastoise",
+    " #010 Caterpie",
+    " #011 Metapod",
+    " #012 Butterfree",
+    " #013 Weedle",
+    " #014 Kakuna",
+    " #015 Beedrill",
+    " #016 Pidgey",
+    " #017 Pidgeotto",
+    " #018 Pidgeot ",
+    " #019 Rattata",
+    " #020 Raticate"];
+ 
+  
+    
+    function searchNameFunction(){
+        
+        //create an space for 5 search results
+        var array = [];
+        var counter=0;
+        // Get the value of the input field with id="pokename"
+       var input = document.getElementById("searchName").value;
+    
+           //find whether there is any letter include in the array
+            for(i=0; i<=19; i++){
+                if(pokemonNameList[i].toString().toLowerCase().includes(input.toString().toLowerCase()) && counter<5){ //use toLowerCase to make this search case insensitive 
+                array.push(pokemonNameList[i]+"\n");
+                counter ++; //counter to limit only 5 search results
+                }
+            }
+    //print the 5 search results by alert()
+        alert(array);
+    }
