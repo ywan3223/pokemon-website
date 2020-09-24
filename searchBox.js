cdd4260c36e621dfbdf81b09b74feca482f98f2f
ywan3223 <wwwinniewang@gmail.com>
2020-09-24 00:01:20 -0400

    finish the number search function

--- searchBox.js
+++ searchBox.js
@@ -6,7 +6,7 @@ function nameFunction() {//check if the input value is avaliable
           return;
         //no more than 20 characters
         } else if (inputname.length > 20) {
-          alert('Input letter should less than 20 characters');
+          alert('Please enter a name less than 20 characters');
           return;
         }
         else{
@@ -40,8 +40,44 @@ function searchNameFunction(){//set a function using the popup box to show the r
     //Take out the comma at the beginning of every line
     var b = arrN.join('');
      alert(b);
+}
+
+function numberFunction(){
+    var inputnumber = document.getElementById('searchNumber').value;
+    if (inputnumber > 20 || inputnumber < 1) {
+        alert('Please enter a number between 1 to 20');
+        return;
+      } 
+    else {
+           searchNumberFunction();
+      }
+}
+function searchNumberFunction(){//set a function using the popup box to show the result of number searching
+    let arrN= []; //set a array to save the name searched result
+    let arrD= [];//set a array to save the description
+    let input = document.getElementById('searchNumber').value; 
+    var max = 0; //set a max variable to control the number of result
+    //set pokemon name and description database
+    let pokemonDes = document.getElementsByClassName('description');
+    let pokemonName = document.getElementsByClassName('name');
+    //set up for loop to find the matched pokemon name with the input letter
+    for (i = 0; i < pokemonName.length; i++) {  
+        if (pokemonName[i].innerHTML.toString().toLowerCase().includes(input.toString())&& max<5){ 
+           arrN.push(pokemonName[i].innerText + "\n");
+           arrD.push(pokemonDes[i].innerText +"\n" +"\n");
+           max++;
+        }
+    }
+//use array to add description into the result array
+for(i=0;i<2*arrD.length;i++){
+    arrN.splice(2*i+1,0,arrD[i]);
+}
+    //Take out the comma at the beginning of every line
+    var b = arrN.join('');
+     alert(b);
 
 }
+
 
 
 
