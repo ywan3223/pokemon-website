c636d7d4febbf055e1a0dcecdb0f92a11f8faa58
ywan3223 <wwwinniewang@gmail.com>
2020-09-23 23:31:15 -0400

    finish name search function and change some css setting

--- searchBox.js
+++ searchBox.js
@@ -1,27 +1,3 @@
-
-//write a list for all pokemon name  
-var pokemonNameList = [
-    " #001 Bulbasaur",
-    " #002 Ivysaur",
-    " #003 Venusaur",
-    " #004 Charmander",
-    " #005 Charmeleon",
-    " #006 Charizard",
-    " #007 Squirtle",
-    " #008 Wartortle",
-    " #009 Blastoise",
-    " #010 Caterpie",
-    " #011 Metapod",
-    " #012 Butterfree",
-    " #013 Weedle",
-    " #014 Kakuna",
-    " #015 Beedrill",
-    " #016 Pidgey",
-    " #017 Pidgeotto",
-    " #018 Pidgeot ",
-    " #019 Rattata",
-    " #020 Raticate"];
-
 function nameFunction() {//check if the input value is avaliable
     var inputname = document.getElementById('searchName').value;
        //only accept A-Z or a-z 
@@ -39,22 +15,33 @@ function nameFunction() {//check if the input value is avaliable
     }
 
 function searchNameFunction(){//set a function using the popup box to show the result
-    //set a array to save the name searched result
-    var arr = [];
-    //set a max variable to control the number of result
-    var max=0;
-    //chang input variables from the search bar to value
-    var input = document.getElementById("searchName").value;
-            
+   
+    let arrN= []; //set a array to save the name searched result
+    let arrD= [];//set a array to save the description
+    let input = document.getElementById('searchName').value;
+    input=input.toLowerCase(); 
+    var max = 0; //set a max variable to control the number of result
+    //set pokemon name and description database
+    let pokemonDes = document.getElementsByClassName('description');
+    let pokemonName = document.getElementsByClassName('name');
     //set up for loop to find the matched pokemon name with the input letter
-    for(i=0; i<=19; i++){
-        if(pokemonNameList[i].toString().toLowerCase().includes(input.toString().toLowerCase()) && max<5){ 
-            arr.push(pokemonNameList[i]+"\n");//push the result from name list to the result list
-            max ++; 
-            }
+    for (i = 0; i < pokemonName.length; i++) {  
+        if (pokemonName[i].innerHTML.toLowerCase().includes(input)&& max<5){ 
+           arrN.push(pokemonName[i].innerText + "\n");
+           arrD.push(pokemonDes[i].innerText +"\n" +"\n");
+           max++;
+        }
     }
-    //wipe the comma betwenn every element of the result array
-    var b = arr.join('');
-    //use the alert popup box to show the result
-    alert(b);
+    //use array to add description into the result array
+    for(i=0;i<2*arrD.length;i++){
+        arrN.splice(2*i+1,0,arrD[i]);
+    }
+    
+    //Take out the comma at the beginning of every line
+    var b = arrN.join('');
+     alert(b);
+
 }
+
+
+
